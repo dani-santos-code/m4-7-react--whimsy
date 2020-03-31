b6e3e9bcc97926adebe673052ab0e4a026eaf861
Dani Santos <santos.adaniele@gmail.com>
2020-03-30 20:26:20 -0400

    implement numOfLikes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,13 +7,28 @@ import avatar from "../../assets/carmen-sandiego.png";
 import Tweet from "../Tweet";
 
 const App = () => {
-  const reducer = (state, action) => {};
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "TOGGLE-LIKE":
+        return {
+          ...state,
+          numOfLikes: state.numOfLikes + 1,
+          isLiked: !state.isLiked
+        };
+      default:
+        throw new Error("Action type not recognized");
+    }
+  };
   const [state, dispatch] = useReducer(reducer, {
     numOfLikes: 100,
     numOfRetweets: 100,
     isLiked: false,
     isRetweeted: false
   });
+
+  const toggleLike = () => {
+    dispatch({ type: "TOGGLE-LIKE" });
+  };
 
   return (
     <Wrapper>
@@ -27,6 +42,7 @@ const App = () => {
         numOfLikes={state.numOfLikes}
         isLikedByCurrentUser={state.isLiked}
         isRetweetedByCurrentUser={state.isRetweeted}
+        handleToggleLike={toggleLike}
       />
     </Wrapper>
   );
